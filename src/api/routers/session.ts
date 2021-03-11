1969eb63759b7b5270b3a2e375b646c910733e9b
HoseungJang <hsjang.dev@gmail.com>
2021-03-12 00:29:19 +0900

    apply account information validation

--- src/api/routers/session.ts
+++ src/api/routers/session.ts
@@ -6,6 +6,7 @@ import { validateParameters } from "../middlewares/validateParameters";
 import { AuthService } from "../../services/auth";
 
 import * as Presenters from "../presenters";
+import { ErrorResponse } from "../../utils/error";
 
 export const applySessionRouters = (rootRouter: Router) => {
   const router = Router();
@@ -13,18 +14,27 @@ export const applySessionRouters = (rootRouter: Router) => {
   router.post(
     "/",
     body("email")
-      .equals(process.env.ADMIN_EMAIL!)
-      .withMessage("invalid email")
+      .isString()
+      .withMessage("email must be string")
       .exists()
       .withMessage("email must be provided"),
     body("password")
-      .equals(process.env.ADMIN_PASSWORD!)
-      .withMessage("invalid password")
+      .isString()
+      .withMessage("password must be string")
       .exists()
       .withMessage("password must be provided"),
     validateParameters,
     async (req, res, next) => {
       try {
+        const email = req.body.email as string;
+        const password = req.body.password as string;
+
+        const isValidAccount = AuthService.verifyAdmin({ email, password });
+
+        if (!isValidAccount) {
+          throw new ErrorResponse(401, "invalid account information");
+        }
+
         const token = AuthService.createJWT();
 
         return res.status(201).json(Presenters.presentSession({ token }));
