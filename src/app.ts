a45a60e0d42c69fca74bb8d0509072221836cd9f
HoseungJang <hsjang.dev@gmail.com>
2021-04-18 04:27:22 +0900

    apply CORS settings

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,12 @@ export class App {
 
     this.app.use(express.json());
 
+    this.app.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+      res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+      next();
+    });
+
     applyAllRouters(this.app);
   }
 
