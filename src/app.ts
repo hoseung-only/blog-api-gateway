73c11daf6a05aaf6dcef18877c3dbe01772e29b9
HoseungJang <hsjang.dev@gmail.com>
2021-05-01 16:21:19 +0900

    allow x-blog-auth-token

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,10 @@ export class App {
 
     this.app.use((req, res, next) => {
       res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-      res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        ["Content-Type", "x-blog-auth-token"].join(", ")
+      );
       next();
     });
 
