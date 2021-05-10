d5bcb29769880a4caceb06ce387bb2dfb5994c43
HoseungJang <hsjang.dev@gmail.com>
2021-05-11 01:43:16 +0900

    use req.headers.origin

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,10 @@ export class App {
     this.app.use(express.json());
 
     this.app.use((req, res, next) => {
+      if (req.headers.origin) {
+        res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+      }
       res.setHeader("Access-Control-Allow-Methods", "*");
-      res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
       res.setHeader(
         "Access-Control-Allow-Headers",
         ["Content-Type", "x-blog-auth-token"].join(", ")
