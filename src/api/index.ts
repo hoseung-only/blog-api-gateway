e44b94c23fdc61b3a7db86037de3e736d1a284e1
HoseungJang <hsjang.dev@gmail.com>
2021-07-22 00:21:49 +0900

    add image router

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,6 +4,7 @@ import { applyOpenAPIRouter } from "./openAPI";
 import { applyPostRouters } from "./routers/post";
 import { applyCategoryRouters } from "./routers/category";
 import { applySessionRouters } from "./routers/session";
+import { applyImageRouters } from "./routers/image";
 import { applyErrorHandlers } from "./errorHandlers";
 
 export function applyAllRouters(app: Application) {
@@ -13,6 +14,7 @@ export function applyAllRouters(app: Application) {
   applyPostRouters(router);
   applyCategoryRouters(router);
   applySessionRouters(router);
+  applyImageRouters(router);
   applyErrorHandlers(router);
 
   app.use("/", router);
---
+++ src/api/routers/image.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import { query } from "express-validator";
+
+import { client } from "@hoseung-only/blog-microservice-sdk";
+
+import { validateParameters } from "../middlewares/validateParameters";
+
+export const applyImageRouters = (rootRouter: Router) => {
+  const router = Router();
+
+  router.get(
+    "/presigned_post",
+    query("fileName").isString().exists(),
+    query("fileType").isString().exists(),
+    validateParameters,
+    async (req, res, next) => {
+      try {
+        const fileName = req.query.fileName as string;
+        const fileType = req.query.fileType as string;
+
+        const response = await client.media.getPresignedPost({ fileName, fileType });
+
+        return res.status(response.statusCode).json(response.body);
+      } catch (error) {
+        return next(error);
+      }
+    }
+  );
+
+  rootRouter.use("/image", router);
+};
